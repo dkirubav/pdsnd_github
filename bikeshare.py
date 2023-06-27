82c2d404452977eac2068706727b584a02c15240
dkirubav <dkirubav@ford.com>
2023-06-27 18:48:33 -0400

    Style: Added a new Welcome message. No Code change

--- bikeshare.py
+++ bikeshare.py
@@ -19,6 +19,7 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
+    print('Hello! Welcome to the Bikeshare Data Project \n')
     print('Hello! Let\'s explore some US bikeshare data! \n')
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     city = input("Enter city name(chicago,new york city,washington:")
